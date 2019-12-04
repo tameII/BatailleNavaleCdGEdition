0fa4278ee6788817b5c1440f73a0f5b42c696d20
BrandonHornbeck <brandon.horneck054@gmail.com>
2019-12-04 10:33:55 +0100

    Board is started

--- src/fr/ul/cdg/model/Board.java
+++ src/fr/ul/cdg/model/Board.java
@@ -1,4 +1,30 @@
 package fr.ul.cdg.model;
 
+import fr.ul.cdg.factory.Era;
+import fr.ul.cdg.factory.Ship;
+
+import java.util.List;
+
 public class Board {
+    private List<Ship> shipList;
+    private Cells cells;
+    public static final int BOARD_SIZE = 10;
+    public static final int EMPTY_CELL = 0;
+
+    private class Cells{
+        int[][] array;
+
+        Cells(){
+            array = new int[BOARD_SIZE][BOARD_SIZE];
+            for(int i = 0; i < BOARD_SIZE; i++){
+                for(int j = 0; j < BOARD_SIZE; j++){
+                    array[i][j] = EMPTY_CELL;        //ALL THE CELLS INITIALISED AS EMPTY
+                }
+            }
+        }
+    }
+    public Board(Era era){
+        shipList = era.shipCreation();
+        cells = new Cells();
+    }
 }
