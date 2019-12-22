c03c2eef6e54436c183938fc26069676a3704894
BrandonHornbeck <brandon.horneck054@gmail.com>
2019-12-22 11:15:07 +0100

    less ammo for renaissance
    Signed-off-by: BrandonHornbeck <brandon.horneck054@gmail.com>

--- src/fr/ul/cdg/factory/renaissance/CaravelRenaissance.java
+++ src/fr/ul/cdg/factory/renaissance/CaravelRenaissance.java
@@ -4,7 +4,7 @@ package fr.ul.cdg.factory.renaissance;
 import fr.ul.cdg.factory.Ship;
 
 public class CaravelRenaissance extends Ship {
-    public static int AMMO_MAX = 20;
+    public static int AMMO_MAX = 10;
     public static int TOTAL_HP = 1;
     public CaravelRenaissance(){
         nbCells = 3;
